ada8b51d8881f0f7ba83411edd1bd2665b62ad94
lo-man13 <ljohnson@westmichiganaviation.org>
2016-09-22 16:47:37 -0400

    new program
    mama

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/stuff.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/stuff.java
@@ -39,6 +39,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+.
 /**
  * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
  * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
